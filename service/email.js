3ddc7471c6574705276f958c9e0822bd04e06d31
Prateek Singh <peekingsurfers01@gmail.com>
2026-06-18 01:18:24 +0530

    fixed smtp issue

--- service/email.js
+++ service/email.js
@@ -2,18 +2,34 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.EMAIL_PASS
+//     },
+//     tls: {
+//         rejectUnauthorized: false
+//     }
+// });
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
+console.log("SMTP_USER:", process.env.SMTP_USER);
+
 // mongosh "mongodb+srv://cluster0.688qkt1.mongodb.net/" --username peekingsurfers01_db_user
+
 
 
 transporter.verify((err, success) => {
